404853979986786073bdef59bfa7619765840adc
Pablo Mussanti <elmussa@hotmail.com>
2019-04-25 21:41:17 -0300

    master

---
+++ Solution-UAI-Vet/Vet.Webside/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vet.Webside.Startup))]
+namespace Vet.Webside
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
