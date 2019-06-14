dc3325f0caeeb83ccda2815e0f7511f28f8bfa96
navegador <navegador@05000203-70900>
2019-06-13 23:12:01 -0300

    Agregado Doctor

---
+++ Solution-UAI-Vet/Vet.Webside/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vet.Webside.Models
+{
+    public class Doctor 
+    {
+ 
+ 
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+            public string Email { get; set; }
+       
+    }
+}
